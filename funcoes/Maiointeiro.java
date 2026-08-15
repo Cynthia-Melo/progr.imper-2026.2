57c5f32f7696c15bfae17a51f94b73d811017820
Cynthia Melo <cynthia.00000860095@unicap.br>
2026-08-15 00:35:38 +0000

    potencia

---
+++ funcoes/Maiointeiro.java
@@ -0,0 +1,24 @@
+package funcoes;
+
+import java.util.Scanner;
+
+public class Maiointeiro {
+
+    public static int maior ( int a, int b){
+        if (a < b ){
+            return a;
+        } else {
+            return b;
+        }
+            
+        }
+    public static void main(String[] args) {
+        Scanner s = new Scanner (system.in);
+        int x = s.nextInt();
+        
+
+    }
+
+}
+
+
