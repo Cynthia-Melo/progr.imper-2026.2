a1c711635affbae87f81796f8595a1ba45b96e14
Cynthia Melo <cynthia.00000860095@unicap.br>
2026-08-23 19:21:26 +0000

    exercício1

---
+++ Lista 1/Conversao.java
@@ -0,0 +1,36 @@
+package funcoes;
+
+import java.util.Scanner;
+
+public class Conversao {
+
+    public static double converte(double c){
+        return c * 1.8 + 32;
+
+    }
+
+    public static double converte1( double c){
+        return c + 273.15;
+
+    }
+
+    public static double converte2( double c){
+        return c * 0.8;
+
+    }
+
+    public static double converte3 ( double c) {
+        return c * 1.8 + 491.67;
+    }
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        double celsius = s.nextDouble();
+
+        System.out.println("Fahrenheit: " + converte(celsius));
+        System.out.println("Kelvin: " + converte1(celsius));
+        System.out.println("Réaumur: " + converte2(celsius));
+        System.out.println("Rankine: " + converte3(celsius));
+
+    }
+}
