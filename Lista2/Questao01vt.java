731a1f9bae88cf7c3baaaabdeabdc2bf4cd07704
Cynthia Melo <cynthia.00000860095@unicap.br>
2026-09-08 23:19:13 +0000

    Inicio da questão 01

---
+++ Lista2/Questao01vt.java
@@ -0,0 +1,9 @@
+public class Questao01vt {
+    public static void main(String[] args) {
+        int[] A = {1, 0, 5, -2, -5, 7};
+
+        int soma = A[0] + A[1] + A[5];
+
+        System.out.println("Soma: " + soma);
+    }
+}
