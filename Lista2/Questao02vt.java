b5e7ce80d0670a75e56ef78e78c53875364a2fe6
Cynthia Melo <cynthia.00000860095@unicap.br>
2026-09-11 23:22:14 +0000

    Resolvendo questoes da Lista 2

--- Lista2/Questao02vt.java
+++ Lista2/Questao02vt.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class q2 {
+public class Questao02vt {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int[] v = new int[6];
