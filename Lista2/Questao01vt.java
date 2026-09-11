d76fd027ce9c0def33d942afa4eb15cfa4b9eb87
Cynthia Melo <cynthia.00000860095@unicap.br>
2026-09-11 23:20:19 +0000

    Resolvendo questoes da Lista 2

--- Lista2/Questao01vt.java
+++ Lista2/Questao01vt.java
@@ -5,5 +5,11 @@ public class Questao01vt {
         int soma = A[0] + A[1] + A[5];
 
         System.out.println("Soma: " + soma);
+        A[4] = 100;
+
+        for (int i=0; i<=6; i++){
+            System.out.println(A[i]);
+        }
+
     }
 }
---
+++ Lista2/q2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] v = new int[6];
+
+        System.out.println("Digite 6 números inteiros");
+        for (int i = 0; i < 6; i++) {
+            v[i] = s.nextInt();
+        }
+
+        System.out.println("\nValores lidos: ");
+        for (int i = 0; i < 6; i++) {
+            System.out.println(v[i]);
+        }
+    }
+}
