68421e5870873373b1f3bf429163791e6b0b7345
Cynthia Melo <cynthia.00000860095@unicap.br>
2026-09-12 00:17:05 +0000

    Aula 11.09

---
+++ Matrizes/SomaLinhasColunas.java
@@ -0,0 +1,14 @@
+package matrizes;
+
+public class SomaLinhascolunas{
+
+    public static void somalinhas( int[][] m){
+        int soma;
+        for( int i = 0; i)
+
+    }
+
+    public static void main(String [] args){
+
+    }
+}
